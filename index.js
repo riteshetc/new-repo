fd532fbe4dff71946e53a4d0d2812f8e617da3da
Ritesh Kumar <riteshankit8757@gmail.com>
2025-08-06 00:39:54 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('Hello, World!');
+console.log('This is the index file for the tut module.');
+console.log('You can add your code here to start building your application.');  
+console.log('Make sure to explore the features and functionalities available in this module.');
+console.log('Happy coding!');
